0e51607e61cc039190692e0ead9bb6b63c00f2d9
il-moostro <elmonstromail@gmail.com>
2023-04-02 07:09:16 +0000

    Make pagination on page load work

--- projectsController.js
+++ projectsController.js
@@ -29,50 +29,93 @@ export class ProjectsController
 
     onPageLoad() {
         const totalProjects = this.requester.getAllProjects()        
-        this.renderProjectsWithPagination(totalProjects)
+        this.renderProjectsInPages(totalProjects)
         this.renderPagination(totalProjects)
+        // this.renderPagination(totalProjects)
     }
 
     cleanProjects() {
         this.projectListElement.innerHTML = "";
     }
 
-    renderProjectsWithPagination(projects) {
+    renderProjectsInPages(projects) {
         const totalPages = Math.ceil(projects.length / this.projectsPerPage);
         for (let i = 0; i < totalPages; i++) {
-            const StartIndex = i * this.projectsPerPage;
-            const EndIndex = StartIndex + this.projectsPerPage;
-            const pageProjects = projects.slice(StartIndex, EndIndex);
-            const projectsPage = document.createElement("div");
-            projectsPage.classList.add("page");
-            projectsPage.dataset.pageNumber = i + 1;
-
-            pageProjects.forEach((pageProject) => {
-                const projectElement = this.projectView.createProjectElement(pageProject);
-                projectsPage.appendChild(projectElement);
-            });
-
-            if (i === 0) {
-                projectsPage.style.display = "block";
-            } else {
-                projectsPage.style.display = "none";
-            }
-            this.projectListElement.appendChild(projectsPage);
+          const StartIndex = i * this.projectsPerPage;
+          const EndIndex = StartIndex + this.projectsPerPage;
+          const pageProjects = projects.slice(StartIndex, EndIndex);
+          const projectsPage = document.createElement("div");
+          projectsPage.classList.add("page");
+          projectsPage.dataset.pageNumber = i + 1;
+        
+          pageProjects.forEach((pageProject) => {
+            const projectElement = this.projectView.createProjectElement(pageProject);
+            projectsPage.appendChild(projectElement);
+          });
+        
+          if (i === 0) {
+            projectsPage.style.display = "flex";
+          } else {
+            projectsPage.style.display = "none";
+          }
+          this.projectListElement.appendChild(projectsPage);
         }
-      
+        
         const allPages = document.querySelectorAll(".page");
-      
+        
         allPages.forEach((page) => {
-        const pageNumber = parseInt(page.dataset.pageNumber);
-      
-            if (pageNumber === 1) {
-                page.style.display = "flex";
-            } else {
-                page.style.display = "none";
-            }
+          const pageNumber = parseInt(page.dataset.pageNumber);
+        
+          if (pageNumber === 1) {
+            page.style.display = "flex";
+          } else {
+            page.style.display = "none";
+          }
         });
       }
       
+      renderPagination(projects) {
+         // Create pagination links
+         const totalPages = Math.ceil(projects.length / this.projectsPerPage);
+         const paginationDiv = this.paginationContainer;
+         paginationDiv.classList.add("pagination");
+         
+         for (let i = 1; i <= totalPages; i++) {
+           const paginationLink = document.createElement("a");
+           paginationLink.href = "#";
+           paginationLink.innerText = i;
+         
+           paginationLink.addEventListener("click", (event) => {
+             event.preventDefault();
+         
+             // Show the clicked page and hide the others
+             const allPages = document.querySelectorAll(".page");
+         
+             allPages.forEach((page) => {
+               if (page.dataset.pageNumber === event.target.innerText) {
+                 page.style.display = "flex";
+               } else {
+                 page.style.display = "none";
+               }
+             });
+         
+             // Update the active link style
+             const allLinks = document.querySelectorAll(".pagination a");
+         
+             allLinks.forEach((link) => {
+               if (link.innerText === event.target.innerText) {
+                 link.classList.add("active");
+               } else {
+                 link.classList.remove("active");
+               }
+             });
+           });
+         
+           paginationDiv.appendChild(paginationLink);
+         }
+         
+         this.projectListElement.appendChild(paginationDiv);
+      }
 
     renderProjects(projectsToRender) {
         projectsToRender.forEach(project => {
@@ -107,12 +150,12 @@ export class ProjectsController
           }
     }
 
-    renderPagination(projectsArray){
-        const totalPages = Math.ceil(projectsArray.length / this.projectsPerPage);
-        this.paginationContainer.innerHTML = "";
-        this.createPaginationButtons(totalPages, projectsArray)
-        this.styleActivePaginationBtn()    
-    }
+    // renderPagination(projectsArray){
+    //     const totalPages = Math.ceil(projectsArray.length / this.projectsPerPage);
+    //     this.paginationContainer.innerHTML = "";
+    //     this.createPaginationButtons(totalPages, projectsArray)
+    //     this.styleActivePaginationBtn()    
+    // }
 
     styleActivePaginationBtn(){
         const buttons=this.container.querySelectorAll('.pagination-button')
